f3bec5c8e6fa37effd84c485b8484ef20c1b9c05
QuanPM <95131155+QuanPM1804@users.noreply.github.com>
2024-04-25 23:04:33 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ public class Member
     public string Birthplace { get; set; }
     public int Age { get; set; }
     public bool IsGraduated { get; set; }
+    public string FullName => LastName + ' ' + FirstName;
 }
 
 public class Program
@@ -24,64 +25,87 @@ public class Program
             new Member { FirstName = "Van C", LastName = "Nguyen", Gender = "Female", DateOfBirth = new DateTime(2002, 1, 1), PhoneNumber = "0123456789", Birthplace = "Ha Noi", Age = 22, IsGraduated = true },
             new Member { FirstName = "Van D", LastName = "Nguyen", Gender = "Male", DateOfBirth = new DateTime(1999, 1, 1), PhoneNumber = "0123456789", Birthplace = "Ha Noi", Age = 25, IsGraduated = true }
         };
-
-        Console.WriteLine("Option: ");
-        Console.WriteLine("1. Return a list of members who are Male");
-        Console.WriteLine("2. Return the oldest one based on 'Age'");
-        Console.WriteLine("3. Return a new list that contains Full Name only ( Full Name = Last Name + First Name)");
-        Console.WriteLine("4. Return 3 lists:");
-        Console.WriteLine("     List of members who have a birth year of 2000");
-        Console.WriteLine("     List of members who have a birth year greater than 2000");
-        Console.WriteLine("     List of members who have a birth year less than 2000");
-        Console.WriteLine("5. Return the first person who was born in Ha Noi.");
-        Console.Write("Enter your choice (1-5): ");
-        int choice = int.Parse(Console.ReadLine());
-        switch (choice)
+        while (true)
         {
-            case 1:
-                var maleMembers = IsMale(members);
-                foreach (var member in maleMembers)
+            try
+            {
+                Console.WriteLine("Option: ");
+                Console.WriteLine("1. Return a list of members who are Male");
+                Console.WriteLine("2. Return the oldest one based on 'Age'");
+                Console.WriteLine("3. Return a new list that contains Full Name only ( Full Name = Last Name + First Name)");
+                Console.WriteLine("4. Return 3 lists:");
+                Console.WriteLine("     List of members who have a birth year of 2000");
+                Console.WriteLine("     List of members who have a birth year greater than 2000");
+                Console.WriteLine("     List of members who have a birth year less than 2000");
+                Console.WriteLine("5. Return the first person who was born in Ha Noi.");
+                Console.Write("Enter your choice (1-5): ");
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
                 {
-                    Console.WriteLine("Name: " + member.LastName + ' ' + member.FirstName);
+                    case 1:
+                        var maleMembers = MemberOperations.GetMaleMembers(members);
+                        foreach (var member in maleMembers)
+                        {
+                            Console.WriteLine("Name: " + member.FullName);
+                        }
+                        break;
+                    case 2:
+                        var oldestMember = MemberOperations.GetOldestMember(members);
+                        Console.WriteLine("The Oldest member: " + oldestMember.FullName);
+                        break;
+                    case 3:
+                        var fullNames = MemberOperations.GetFullNames(members);
+                        foreach (var fullName in fullNames)
+                        {
+                            Console.WriteLine("Full Name: " + fullName);
+                        }
+                        break;                    
+                    case 4:
+                        var listsByBirthYear = MemberOperations.SplitByBirthYear(members);
+                        Console.WriteLine("Members born in 2000:");
+                        foreach (var member in listsByBirthYear.Item1)
+                        {
+                            Console.WriteLine("Name: " + member.FullName);
+                        }
+                        Console.WriteLine("Members born after 2000:");
+                        foreach (var member in listsByBirthYear.Item2)
+                        {
+                            Console.WriteLine("Name: " + member.FullName);
+                        }
+                        Console.WriteLine("Members born before 2000:");
+                        foreach (var member in listsByBirthYear.Item3)
+                        {
+                            Console.WriteLine("Name: " + member.FullName);
+                        }
+                        break;
+                    case 5:
+                        var firstBornInHaNoi = MemberOperations.GetFirstBornInHaNoi(members);
+                        if (firstBornInHaNoi != null)
+                        {
+                            Console.WriteLine(firstBornInHaNoi.FullName);
+                        }
+                        else
+                        {
+                            Console.WriteLine("No member was born in Ha Noi.");
+                        }
+                        break;
+                
+                    default:
+                        Console.WriteLine("Invalid choice. Please enter a number between 1 and 5.");
+                        break;
                 }
-                break;
-            case 2:
-                var OldestMember = Oldest(members);
-                Console.WriteLine("The Oldest: " + OldestMember.LastName + ' ' + OldestMember.FirstName);
-                break;
-            case 3:
-                var FullNameOfMember = GetFullNameOfMembers(members);
-                foreach (var member in FullNameOfMember)
-                {
-                    Console.WriteLine("Full Name: " + member.FullName);
-                }
-                break;
-            case 4:
-                var listsByBirthYear = YearOfBirth(members);
-                Console.WriteLine("Members born in 2000:");
-                foreach (var member in listsByBirthYear.Item1)
-                {
-                    Console.WriteLine("Name: " + member.LastName + ' ' + member.FirstName);
-                }
-                Console.WriteLine("Members born after 2000:");
-                foreach (var member in listsByBirthYear.Item2)
-                {
-                    Console.WriteLine("Name: " + member.LastName + ' ' + member.FirstName);
-                }
-                Console.WriteLine("Members born before 2000:");
-                foreach (var member in listsByBirthYear.Item3)
-                {
-                    Console.WriteLine("Name: " + member.LastName + ' ' + member.FirstName);
-                }
-                break;
-            case 5:
-                var FirstBornInHaNoi = BornInHaNoi(members);
-                Console.WriteLine(FirstBornInHaNoi.LastName + ' ' + FirstBornInHaNoi.FirstName);
-                break;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid input!");
+            }
         }
     }
-
-    public static List<Member> IsMale(List<Member> members)
+}
+public class MemberOperations
+{
+    public static List<Member> GetMaleMembers(List<Member> members)
     {
         List<Member> maleMembers = new List<Member>();
         foreach (var member in members)
@@ -94,53 +118,53 @@ public class Program
         return maleMembers;
     }
 
-    public static Member Oldest(List<Member> members)
+    public static Member GetOldestMember(List<Member> members)
     {
-        var OldestMember = members[0];
+        var oldestMember = members[0];
         foreach (var member in members)
         {
-            if (member.DateOfBirth < OldestMember.DateOfBirth)
+            if (member.DateOfBirth < oldestMember.DateOfBirth)
             {
-                OldestMember = member;
+                oldestMember = member;
             }
         }
-        return OldestMember;
+        return oldestMember;
     }
 
-    public static List<FullNameOfMember> GetFullNameOfMembers(List<Member> members)
+    public static List<string> GetFullNames(List<Member> members)
     {
-        List<FullNameOfMember> fullNames = new List<FullNameOfMember>();
+        List<string> fullNames = new List<string>();
         foreach (var member in members)
         {
-            fullNames.Add(new FullNameOfMember { FullName = $"{member.LastName} {member.FirstName}" });
+            fullNames.Add(member.FullName);
         }
         return fullNames;
     }
 
-    public static Tuple<List<Member>, List<Member>, List<Member>> YearOfBirth(List<Member> members)
+    public static Tuple<List<Member>, List<Member>, List<Member>> SplitByBirthYear(List<Member> members)
     {
-        List<Member> BornIn2000 = new List<Member>();
-        List<Member> BornAfter2000 = new List<Member>();
-        List<Member> BornBefore2000 = new List<Member>();
+        List<Member> bornIn2000 = new List<Member>();
+        List<Member> bornAfter2000 = new List<Member>();
+        List<Member> bornBefore2000 = new List<Member>();
         foreach (var member in members)
         {
             switch (member.DateOfBirth.Year)
             {
                 case var year when year > 2000:
-                    BornAfter2000.Add(member);
+                    bornAfter2000.Add(member);
                     break;
                 case var year when year == 2000:
-                    BornIn2000.Add(member);
+                    bornIn2000.Add(member);
                     break;
                 case var year when year < 2000:
-                    BornBefore2000.Add(member);
+                    bornBefore2000.Add(member);
                     break;
             }
         }
-        return Tuple.Create(BornIn2000, BornAfter2000, BornBefore2000);
+        return Tuple.Create(bornIn2000, bornAfter2000, bornBefore2000);
     }
 
-    public static Member BornInHaNoi(List<Member> members)
+    public static Member GetFirstBornInHaNoi(List<Member> members)
     {
         foreach (var member in members)
         {
@@ -151,9 +175,4 @@ public class Program
         }
         return null;
     }
-}
-
-public class FullNameOfMember
-{
-    public string FullName { get; set; }
 }
